7815e94e1c87736a4a6eb44fdf27589853f56fa5
johnny-bitis <51972715+johnny-bitis@users.noreply.github.com>
2020-02-08 04:18:18 -0400

    rota para editar registro

--- play-java/app/domains/person/controllers/PersonController.java
+++ play-java/app/domains/person/controllers/PersonController.java
@@ -29,15 +29,23 @@ public class PersonController extends Controller {
         this.ec = ec;
     }
 
-    public Result index(final Http.Request request) {
+    public Result index(final Http.Request request, String message) {
+        return ok(index.render(request,message));
+    }
+
+    public Result edit(final Http.Request request, Long id) {
         return ok(index.render(request,""));
     }
 
-    public CompletionStage<Result> addPerson(final Http.Request request) {
+    public CompletionStage<Result> create(final Http.Request request) {
         Person person = formFactory.form(Person.class).bindFromRequest(request).get();
         return personRepository
                 .add(person)
-                .thenApplyAsync(p -> ok(index.render(request,"Registro Salvo")), ec.current());
+                .thenApplyAsync(p -> this.index(request,"Registro salvo"), ec.current());
+    }
+
+    public Result update(final Http.Request request) {
+        return ok(index.render(request,"teste"));
     }
 
     public CompletionStage<Result> getPeople() {
